def94039208d79e077d6124a0dcdcb05bc24bb3e
codepilot2010 <93812156+codepilot2010@users.noreply.github.com>
2022-08-09 12:26:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,4 +12,21 @@ recognition.onresult=function(event)
     var content=event.results[0][0].transcript;
     console.log(content);
     document.getElementById("textbox").innerHTML=content;
+    speak();
 }
+function speak()
+{
+    var synth=window.speechSynthesis;
+    speak_data=document.getElementById("textbox").value;
+    var utterthis=new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterthis);
+    Webcam.attach(camera);
+}
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:'png',
+    png_quality:90
+
+});
+camera=document.getElementById("camera");
