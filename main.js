87d08927050908ee82e1a91e15bf156d677b403d
codepilot2010 <93812156+codepilot2010@users.noreply.github.com>
2022-08-13 12:31:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,15 +12,24 @@ recognition.onresult=function(event)
     var content=event.results[0][0].transcript;
     console.log(content);
     document.getElementById("textbox").innerHTML=content;
-    speak();
+    if(content=="take my selfie")
+    {
+        console.log("taking selfie---");
+        speak();
+    }
 }
 function speak()
 {
     var synth=window.speechSynthesis;
-    speak_data=document.getElementById("textbox").value;
+    speak_data="taking your selfie in 5 seconds";
     var utterthis=new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterthis);
     Webcam.attach(camera);
+    setTimeout(function()
+    {
+        take_snapshot();
+        save();
+    },5000);
 }
 Webcam.set({
     width:360,
@@ -30,3 +39,17 @@ Webcam.set({
 
 });
 camera=document.getElementById("camera");
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri)
+    {
+        document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_uri+'">';
+    });
+}
+function save()
+{
+    link=document.getElementById("link");
+    image=document.getElementById("selfie_image").src;
+    link.href=image;
+    link.click();
+}
